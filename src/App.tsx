5f09fb7bb42f3dca1d0552310e248ae1e19398e5
hadi arbabi <hadimojak@gmail.com>
2025-01-22 18:17:37 +0330

    todo list 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
+  const todos = [{ id: "t1", text: "finished the course" }];
   // const App: JSX.Element = () => {
   return (
     <div className="App">
-      <TodoList />
+      {/* { component that add todos} */}
+      <TodoList items={todos} />
     </div>
   );
 };
